959da40c994fb5243b2b60e91fbfb0ce835f93c9
Ted <tedlee1444@gmail.com>
2026-01-13 15:01:58 +0900

    test2

--- src/students/students.controller.spec.ts
+++ src/students/students.controller.spec.ts
@@ -1,15 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
+import { Student } from './entities/student.entity';
+import { getRepositoryToken } from '@nestjs/typeorm/dist/common/typeorm.utils';
 
 describe('StudentsController', () => {
   let controller: StudentsController;
+  let mockRepo: any;
 
   // test 세팅
   beforeEach(async () => {
+    mockRepo = {
+      create: jest.fn(),
+      save: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StudentsController],
-      providers: [StudentsService],
+      providers: [
+        StudentsService,
+        { useValue: mockRepo, provide: getRepositoryToken(Student) },
+      ],
     }).compile();
 
     controller = module.get<StudentsController>(StudentsController);
